467262302c309c24fc658884e4de28eae7800f63
Matthias Blochberger <matthias.blochberger@posteo.at>
2022-04-21 14:05:37 +0200

    final random sim hopefully

--- python/simulation_random.py
+++ python/simulation_random.py
@@ -18,7 +18,7 @@ L = int(sys.argv[4])
 print("Job: %d, SNR: %d, N_sens: %d, L: %d" % (process_id, SNR, N_sens, L))
 
 # %%
-runs = 50
+runs = 30
 plot = False
 
 # %%
@@ -27,7 +27,7 @@ N_f = 1024
 # N_sens = 3
 
 # %%
-N_s = 20000
+N_s = 8000 * 100
 
 # %%
 seed = 12345
@@ -92,7 +92,8 @@ for run in range(runs):
         network_newton_fq.setConnection(name, connections)
         network_newton_fq_diag.setConnection(name, connections)
 
-    # %% GENERATE AR SIGNAL
+    # %%
+    # GENERATE AR SIGNAL
     N_ff = 1025
     b1 = signal.firwin(N_ff, 0.25, pass_zero=False)
     M = 20
@@ -124,9 +125,10 @@ for run in range(runs):
         x[k, :, None] = H @ s_[k : k + L][::-1] + n_var * rng.normal(size=(N_sens, 1))
 
     # %%
-    hopsize = 1
+    hopsize = L
 
-    # %% CENTRALIZED TIME DOMAIN QUASI NEWTON METHOD
+    # %%
+    # CENTRALIZED TIME DOMAIN QUASI NEWTON METHOD
     print("CENTRALIZED TIME DOMAIN QUASI NEWTON METHOD")
     err_MCQN = []
     rho = 0.5  # step size
@@ -141,7 +143,8 @@ for run in range(runs):
         err_MCQN.append(bsi_mcqn.get_error(h))
     err_MCQN = np.asarray(err_MCQN)
 
-    # %% CENTRALIZED NORMALIZED FREQUENCY DOMAIN LMS METHOD
+    # %%
+    # CENTRALIZED NORMALIZED FREQUENCY DOMAIN LMS METHOD
     print("CENTRALIZED NORMALIZED FREQUENCY DOMAIN LMS METHOD")
     err_NMCFLMS = []
     rho = 0.4  # step size
@@ -153,7 +156,8 @@ for run in range(runs):
         err_NMCFLMS.append(bsi_nmcflms.get_error(h))
     err_NMCFLMS = np.asarray(err_NMCFLMS)
 
-    # %% CENTRALIZED ROBUST NORMALIZED FREQUENCY DOMAIN LMS METHOD
+    # %%
+    # CENTRALIZED ROBUST NORMALIZED FREQUENCY DOMAIN LMS METHOD
     print("CENTRALIZED ROBUST NORMALIZED FREQUENCY DOMAIN LMS METHOD")
     err_RNMCFLMS = []
     rho = 0.25  # step size
@@ -166,7 +170,8 @@ for run in range(runs):
         err_RNMCFLMS.append(bsi_rnmcflms.get_error(h))
     err_RNMCFLMS = np.asarray(err_RNMCFLMS)
 
-    # %% CENTRALIZED l_p NORM CONSTRAINED ROBUST NORMALIZED FREQUENCY DOMAIN LMS METHOD
+    # %%
+    # CENTRALIZED l_p NORM CONSTRAINED ROBUST NORMALIZED FREQUENCY DOMAIN LMS METHOD
     print(
         "CENTRALIZED l_p NORM CONSTRAINED ROBUST NORMALIZED FREQUENCY DOMAIN LMS METHOD"
     )
@@ -182,7 +187,8 @@ for run in range(runs):
         err_LPRNMCFLMS.append(bsi_lprnmcflms.get_error(h))
     err_LPRNMCFLMS = np.asarray(err_LPRNMCFLMS)
 
-    # %% DISTRIBUTED NEWTON RANK-1 TIME DOMAIN METHOD
+    # %%
+    # DISTRIBUTED NEWTON RANK-1 TIME DOMAIN METHOD
     print("DISTRIBUTED NEWTON RANK-1 TIME DOMAIN METHOD")
     rho_admm = 0.05  # penalty parameter / step size
     mu = 0.05  # newton step size
@@ -201,11 +207,13 @@ for run in range(runs):
         err_td.append(error)
     err_td = np.asarray(err_td)
 
-    # %% DISTRIBUTED NEWTON FREQUENCY DOMAIN METHOD
+    # %%
+    # DISTRIBUTED NEWTON FREQUENCY DOMAIN METHOD
     print("DISTRIBUTED NEWTON FREQUENCY DOMAIN METHOD")
-    rho_admm = 0.1
+    hopsize = L
+    rho_admm = 1
     mu = 0.2
-    eta = 0.0
+    eta = 0.98
     err_ADMM_newton_fq = []
     h_test_newton_fq = np.zeros(shape=h.shape)
     network_newton_fq.setBufferSize(1)
@@ -227,10 +235,12 @@ for run in range(runs):
         err_ADMM_newton_fq.append(error)
     err_ADMM_newton_fq = np.asarray(err_ADMM_newton_fq)
 
-    # %% DISTRIBUTED DIAGONALIZED NEWTON FREQUENCY DOMAIN METHOD
+    # %%
+    # DISTRIBUTED DIAGONALIZED NEWTON FREQUENCY DOMAIN METHOD
     print("DISTRIBUTED DIAGONALIZED NEWTON FREQUENCY DOMAIN METHOD")
+    hopsize = L
     rho_admm = 1
-    mu = 0.5
+    mu = 0.6
     eta = 0.98
     err_ADMM_newton_fq_diag = []
     h_test_newton_fq_diag = np.zeros(shape=h.shape)
@@ -279,7 +289,7 @@ if plot:
     plt.plot(20 * np.log10(err_td), label="ADMM")
     plt.plot(20 * np.log10(err_ADMM_newton_fq), label="ADMM FQ")
     plt.plot(20 * np.log10(err_ADMM_newton_fq_diag), label="ADMM FQ DIAG")
-    plt.plot(np.ones_like(err_MCQN) * (-SNR), "--k", label="Noise floor")
+    # plt.plot(np.ones_like(err_MCQN) * (-SNR), "--k", label="Noise floor")
     plt.title("Misalignment")
     plt.xlabel("Frame [1]")
     plt.ylabel("Normalized Misalignment NPM [dB]")
