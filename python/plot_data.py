145d470a4457ec9f413ceaa335caf2bbbef2bb91
Matthias Blochberger <matthias.blochberger@posteo.at>
2022-04-22 10:41:03 +0200

    experiments 1 plots

--- python/plot_data.py
+++ python/plot_data.py
@@ -192,32 +192,63 @@ for SNR in SNRs:
     )
 
 # %%
-fig = plt.figure(figsize=(5, 3))
+fig = plt.figure(figsize=(3, 2.5))
 # plt.plot(SNRs, -np.asarray(SNRs), "k--", label="SNR")
 plt.plot(SNRs, 20 * np.log10(err_MCQN_avg), marker="o", label="MCQN")
 plt.plot(SNRs, 20 * np.log10(err_NMCFLMS_avg), marker=">", label="NMCFLMS")
-plt.plot(SNRs, 20 * np.log10(err_RNMCFLMS_avg), marker="o", label="RNMCFLMS")
-plt.plot(SNRs, 20 * np.log10(err_LPRNMCFLMS_avg), marker="^", label="LPRNMCFLMS")
+# plt.plot(SNRs, 20 * np.log10(err_RNMCFLMS_avg), marker="o", label="RNMCFLMS")
+plt.plot(SNRs, 20 * np.log10(err_LPRNMCFLMS_avg), marker="^", label=r"$l_p$-RNMCFLMS")
 # plt.plot(SNRs, 20 * np.log10(err_ADMM_newton_td_avg), marker="v", label="ADMM")
 # plt.plot(SNRs, 20 * np.log10(err_ADMM_newton_fq_avg), marker="o", label="ADMM FQ")
 plt.plot(
     SNRs,
     20 * np.log10(err_ADMM_newton_fq_diag_avg),
     marker="s",
-    label="ADMM FQ DIAG",
+    label="ADMM BSI",
 )
 # plt.title("Median misalignment after 8000 Samples")
 plt.xlabel("SNR [dB]")
 plt.ylabel("Average NPM [dB]")
 plt.xticks(SNRs)
-# plt.yticks(-SNRs)
+plt.xlim(3,52)
+plt.ylim(-85,-10)
+# plt.yticks(-np.asarray(SNRs))
 plt.grid()
-plt.legend(fontsize="small", ncol=2)
+plt.legend(fontsize="small")
 plt.tight_layout()
 plt.show()
 
 # %%
 utils.savefig(fig, "NPM_over_SNR_L%d_M%d" % (L_p, N_p))
+
+# %%
+fig = plt.figure(figsize=(3, 2.5))
+SNR = 10
+if err_MCQN_[SNR] is not None:
+    plt.plot(20 * np.log10(np.nanmedian(err_MCQN_[SNR], axis=0)[:-L]), label="MCQN")
+if err_NMCFLMS_[SNR] is not None:
+    plt.plot(
+        20 * np.log10(np.nanmedian(err_NMCFLMS_[SNR], axis=0)[:-L]), label="NMCFLMS"
+    )
+if err_LPRNMCFLMS_[SNR] is not None:
+    plt.plot(
+        20 * np.log10(np.nanmedian(err_LPRNMCFLMS_[SNR], axis=0)[:-L]),
+        label=r"$l_p$-RNMCFLMS",
+    )
+if err_ADMM_newton_fq_diag_[SNR] is not None:
+    plt.plot(
+        20 * np.log10(np.nanmedian(err_ADMM_newton_fq_diag_[SNR], axis=0)[:-L]),
+        label="ADMM FQ DIAG",
+    )
+plt.xlabel("Frame [1]")
+plt.ylabel("NPM [dB]")
+plt.xlim(0,8000)
+plt.ylim(-40,0)
+plt.grid()
+plt.legend(fontsize="small")
+plt.tight_layout()
+plt.show()
+utils.savefig(fig, "NPM_over_time_SNR%d" % (SNR))
 
 # %% random connectivity
 SNRs = [20]
